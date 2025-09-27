26de53c36253d7110a5b2de49c8c3219a4171b84
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-09-27 13:41:40 +0100

    test(builder): add builder spec

---
+++ spec/lib/builder_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+require_relative '../../lib/builder'
+
+describe SSG::Builder do
+  describe '.build' do
+    let(:renderer) { instance_double(SSG::PageRenderer) }
+
+    before do
+      allow(FileUtils).to receive(:rm_rf).once
+      allow(FileUtils).to receive(:mkdir_p).once
+      allow(SSG::LayoutLoader).to receive(:load_all).and_return({}).once
+      allow(SSG::PageProcessor).to receive(:process_all).and_return({}).once
+      allow(SSG::PageRenderer).to receive(:new).and_return(renderer)
+      allow(renderer).to receive(:render_all).once
+      allow(SSG::AssetCopier).to receive(:copy_assets).once
+    end
+
+    describe 'successful build' do
+      it 'builds successfully' do
+        SSG::Builder.build
+      end
+    end
+
+    describe 'build with error' do
+      before do
+        allow(SSG::LayoutLoader).to receive(:load_all)
+          .and_raise(SSG::SSGError, 'Layout load error')
+      end
+
+      it 'handles errors gracefully' do
+        expect {
+          SSG::Builder.build
+        }.not_to raise_error
+      end
+    end
+  end
+end

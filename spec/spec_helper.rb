e261c9c7a2405e0fdff71f2fc6365a3455966717
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-10-04 12:23:45 +0100

    feat(rspec): remove EventLogger output during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,11 @@ require_relative 'helper_methods'
 RSpec.configure do |config|
   config.include HelperMethods
 
+  # Suppress EventLogger output during tests
+  config.before do
+    stub_const('SSG::EventLogger::LOG_OUTPUT', File::NULL)
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.

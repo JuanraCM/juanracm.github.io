51afdbb2c9325fde0eb5c8c2332e1bbc0ae26020
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-10-14 10:45:52 +0100

    test(view_context): add view_context specs

---
+++ spec/lib/ssg/view_context_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SSG::ViewContext do
+  subject(:view_context) { described_class.new(layouts, page_data) }
+
+  let(:layouts) do
+    {
+      'base' => File.read(fixture_path('layouts/base.html.erb')),
+      'default' => File.read(fixture_path('layouts/default.html.erb')),
+      'home' => File.read(fixture_path('layouts/home.html.erb'))
+    }
+  end
+  let(:page_data) do
+    {
+      config: {
+        layout: 'home',
+        title: 'Test Page'
+      },
+      content: '<p>This is a test page</p>'
+    }
+  end
+
+  describe '#render' do
+    subject(:rendered_output) { view_context.render }
+
+    context 'when a parent layout is specified' do
+      it 'includes content from the parent layout' do
+        expect(rendered_output).to include('<title>Test Page</title>')
+      end
+
+      it 'includes content from the page layout' do
+        expect(rendered_output).to include('<h1>Welcome to Our Website</h1>')
+      end
+
+      it 'includes page content' do
+        expect(rendered_output).to include('<p>This is a test page</p>')
+      end
+    end
+
+    context 'when a parent layout is not specified' do
+      before do
+        page_data[:config][:layout] = 'default'
+      end
+
+      it 'includes content from the page layout' do
+        expect(rendered_output).to include('<h1>This is the default layout</h1>')
+      end
+
+      it 'includes page content' do
+        expect(rendered_output).to include('<p>This is a test page</p>')
+      end
+    end
+  end
+end

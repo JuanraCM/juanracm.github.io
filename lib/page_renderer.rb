b0cc8fd64ae784554e24de4f70695cc1da9cbcd1
Juan Ramón Carretero Martín <juanracm.dev@gmail.com>
2025-10-14 21:39:50 +0200

    feat(layouts): parent layouts (#1)
    * feat(view_renderer): added ViewContext class
    
    * refactor(layouts): default layout with common HTML
    
    * fix(rubocop): unwanted space
    
    * refactor(page_renderer): rename exceptions for better readability
    
    * test(view_context): add view_context specs
    
    * fix(view_context): ensure layout is valid when rendering template
    
    * fix(tests): remove outdated page_renderer test case
    
    ---------
    
    Co-authored-by: Juan Ramón Carretero Martín <jrcarretero@tirant.com>

--- lib/page_renderer.rb
+++ lib/page_renderer.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-require 'erb'
-
 require_relative 'config'
 require_relative 'hot_reload'
+require_relative 'view_context'
 
 module SSG
   class PageRenderer
     class MissingLayoutError < SSGError; end
-    class MissingTemplateError < SSGError; end
 
     def initialize(layouts)
       @layouts = layouts
@@ -25,28 +23,18 @@ module SSG
     def render_page(page_path, page_data)
       layout_name = page_data[:config][:layout]
       raise_missing_layout_error(page_path) unless layout_name
-      raise_missing_template_error(layout_name, page_path) unless @layouts.key?(layout_name)
 
-      template = ERB.new(@layouts[layout_name])
-      template_result = template.result_with_hash(
-        site: SiteConfig,
-        meta: page_data[:config],
-        content: page_data[:content]
-      )
-      SSG::HotReload.inject_html_snippet(template_result)
+      rendered_page = ViewContext.new(@layouts, page_data).render
+      HotReload.inject_html_snippet(rendered_page)
 
       output_path = File.join(BUILD_DIR, "#{page_path}.html")
 
       FileUtils.mkdir_p(File.dirname(output_path))
-      File.write(output_path, template_result)
+      File.write(output_path, rendered_page)
     end
 
     def raise_missing_layout_error(path)
       raise MissingLayoutError, "Layout not specified for path: #{path}"
-    end
-
-    def raise_missing_template_error(layout, path)
-      raise MissingTemplateError, "Layout '#{layout}' not found for #{path}"
     end
   end
 end
---
+++ lib/view_context.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'erb'
+
+require_relative 'config'
+require_relative 'site_config'
+
+module SSG
+  class ViewContext
+    class InvalidLayoutError < SSGError; end
+
+    attr_reader :meta, :content
+
+    def initialize(layouts, page_data)
+      @layouts = layouts
+      @meta = page_data[:config] || {}
+      @content = page_data[:content] || ''
+    end
+
+    def render
+      render_result = render_template(@meta[:layout])
+
+      @layout_result || render_result
+    end
+
+    private
+
+    def layout(name, page_class: '', &block)
+      @page_class = page_class
+      @layout_result = render_template(name, &block)
+    end
+
+    def render_template(template_name)
+      layout = @layouts[template_name]
+      raise InvalidLayoutError, "Layout '#{template_name}' not found" unless layout
+
+      template = ERB.new(layout)
+      template.result(binding)
+    end
+
+    def site
+      SiteConfig
+    end
+
+    def page_class
+      @page_class || ''
+    end
+  end
+end

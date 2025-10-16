8b78a6d0c74152acb995d79a0b55217fc4c839e4
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-10-16 18:08:53 +0100

    feat(layouts): yield_content helper

--- lib/view_context.rb
+++ lib/view_context.rb
@@ -15,6 +15,8 @@ module SSG
       @layouts = layouts
       @meta = page_data[:config] || {}
       @content = page_data[:content] || ''
+
+      @_render_blocks = {}
     end
 
     def render
@@ -30,12 +32,34 @@ module SSG
       @layout_result = render_template(name, &block)
     end
 
-    def render_template(template_name)
+    def yield_content(name = :body, &block)
+      if block_given?
+        @_render_blocks[name] = capture(&block)
+      else
+        @_render_blocks[name]
+      end
+    end
+
+    def render_template(template_name, &block)
       layout = @layouts[template_name]
       raise InvalidLayoutError, "Layout '#{template_name}' not found" unless layout
 
+      @_render_blocks[:body] = capture(&block) if block_given?
+
       template = ERB.new(layout)
       template.result(binding)
+    end
+
+    def capture(&block)
+      buf_was = block.binding.local_variable_get(:_erbout)
+      out_buf = +''
+
+      block.binding.local_variable_set(:_erbout, out_buf)
+      block.call
+
+      out_buf
+    ensure
+      block.binding.local_variable_set(:_erbout, buf_was)
     end
 
     def site

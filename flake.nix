2ff27f0c77782b893e0b2e0ae7a482ddf18a9978
JuanraCM <juanracm.dev@gmail.com>
2026-01-04 14:00:36 +0000

    feat(flake): added nix devShell flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development environment for Personal Blog";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          ruby
+          rendercv
+        ];
+
+        shellHook = ''
+          echo "Refreshing dependencies..."
+          bundle install
+        '';
+      };
+    };
+}
